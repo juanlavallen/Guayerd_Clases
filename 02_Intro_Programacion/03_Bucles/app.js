4fe3248d14a42f3479433aa179bfc035d58db00e
JuanLavallen <juanlavallen02@gmail.com>
2021-10-30 22:12:33 -0300

    03_Bucles JS

---
+++ 02_Intro_Programacion/03_Bucles/app.js
@@ -0,0 +1,41 @@
+// Bucle FOR
+
+alert('Esto es el alert 1');
+alert('Esto es el alert 2');
+alert('Esto es el alert 3');
+alert('Esto es el alert 4');
+alert('Esto es el alert 5');
+
+// Hacemos lo mismo pero usando un FOR
+
+// Usamos VAR
+
+for(var i = 1; i < 6; i++) {
+    alert('Esto es el alert ' + i);
+}
+
+// Usamos LET
+
+for(let i = 0; i < 6 ; i++){
+    alert('Esto es el alert ' + i);
+}
+
+/*
+variable++ = el simbolo ++ a la derecha, en JS suma un entero a un valor numerico(1)
+*/
+
+// Bucle WHILE
+let texto = '';
+let i = 0;
+
+while(i < 10){
+    texto += 'El numero es: ' + i + '\n';
+    alert(texto);
+    i++
+}
+
+do {
+    texto += 'El numero es: ' + i + '\n';
+    alert(texto);
+    i++;
+} while(i < 10);
