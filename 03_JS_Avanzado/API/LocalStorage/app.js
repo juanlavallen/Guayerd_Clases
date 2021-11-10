ea19afe7da10cfa9891407c8babf8ae48ee6e07d
Juan Lavallen <juanlavallen02@gmail.com>
2021-11-10 12:19:33 -0300

    first commit

---
+++ 03_JS_Avanzado/API/LocalStorage/app.js
@@ -0,0 +1,16 @@
+/****** Session Storage *******/
+sessionStorage.setItem("nombre", "Juan");
+sessionStorage.setItem("apellido", "Lavallen");
+
+document.getElementById("nombre").innerHTML = sessionStorage.getItem("nombre");
+document.getElementById("apellido").innerHTML = sessionStorage.getItem("apellido");
+
+/****** Local Storage ******/
+if(typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("prueba", "Esto es una prueba");
+    // Devolucion
+    document.getElementById("prueba").innerHTML = localStorage.getItem("prueba");
+} else {
+    document.getElementById("prueba").innerHTML = "Tu navegador no soporta Web Storage";
+}
